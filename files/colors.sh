29e2d79b97c0f1ea900bb058f83ce8fe8c36268f
Patrick <tradiuz@gmail.com>
2025-08-22 18:44:10 +0000

    hopefully this is it

--- files/colors.sh
+++ files/colors.sh
@@ -70,3 +70,7 @@ On_IBlue="\e[0;104m"    # Blue
 On_IPurple="\e[0;105m"  # Purple
 On_ICyan="\e[0;106m"    # Cyan
 On_IWhite="\e[0;107m"   # White
+
+# Extra Colors
+Orange="\e[38;5;214m"
+LYellow="\e[38;5;229m"
